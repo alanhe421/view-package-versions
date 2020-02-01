4e45668e85451c8336caab6db32c4572a8f64057
alanhg <qianghe421@gmail.com>
2020-02-01 15:52:12 +0800

    refactor use GeneralCommandLine

--- src/cn/alanhe/ViewPackageVersions.java
+++ src/cn/alanhe/ViewPackageVersions.java
@@ -2,6 +2,9 @@ package cn.alanhe;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.intellij.execution.ExecutionException;
+import com.intellij.execution.configurations.GeneralCommandLine;
+import com.intellij.execution.process.ScriptRunnerUtil;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
@@ -23,10 +26,9 @@ import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.lang.reflect.Type;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
@@ -53,9 +55,9 @@ public class ViewPackageVersions extends AnAction {
         assert firstParent != null;
         String packageName = getPackageName((LeafPsiElement) firstParent);
         try {
-            List<String> versions = this.getPackageVersions(packageName);
+            List<String> versions = this.getPackageVersions(packageName, project);
             this.showPopup(packageName, versions, editor);
-        } catch (IOException | InterruptedException ex) {
+        } catch (ExecutionException ex) {
             ex.printStackTrace();
         }
     }
@@ -66,22 +68,18 @@ public class ViewPackageVersions extends AnAction {
         return name.substring(1, name.length() - 1);
     }
 
-    public List<String> getPackageVersions(String name) throws IOException,
-            InterruptedException {
-        final String command = "npm view " + name + " versions --json";
-        Process p = Runtime.getRuntime().exec(command);
-        p.waitFor();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(
-                p.getInputStream()));
-        String line;
-        StringBuilder builder = new StringBuilder();
-        while ((line = reader.readLine()) != null) {
-            builder.append(line);
-        }
+    public List<String> getPackageVersions(String name, Project project) throws ExecutionException {
+        ArrayList<String> cmds = new ArrayList<>();
+        cmds.add("npm");
+        GeneralCommandLine generalCommandLine = new GeneralCommandLine(cmds);
+        generalCommandLine.setCharset(Charset.forName("UTF-8"));
+        generalCommandLine.setWorkDirectory(project.getBasePath());
+        generalCommandLine.addParameters("view", name, "versions", "--json");
+        String commandLineOutputStr = ScriptRunnerUtil.getProcessOutput(generalCommandLine);
+        Gson converter = new Gson();
         Type type = new TypeToken<List<String>>() {
         }.getType();
-        Gson converter = new Gson();
-        List<String> result = converter.fromJson(builder.toString(), type);
+        List<String> result = converter.fromJson(commandLineOutputStr, type);
         Collections.reverse(result);
         return result;
     }
